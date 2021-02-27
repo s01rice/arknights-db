c322f550a6bec13abe000a3780af826dd2adc1fb
s01rice <s01rice@hotmail.ca>
2021-02-27 02:35:36 -0500

    filter prototypes

--- src/App.js
+++ src/App.js
@@ -7,28 +7,69 @@ import Modal from './Modal.js';
 const baseUrl = "https://gamepress.gg"
 
 class App extends React.Component {
-  state = {
-    show: false,
-    operator: null
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      selectedOp: null,
+      filterTags: {
+        class: {
+          Vanguard: false,
+          Guard: false,
+          Defender: false,
+          Medic: false,
+          Caster: false,
+          Supporter: false,
+          Sniper: false,
+          Specialist: false
+        },
+        rarity: {
+          6: false,
+          5: false,
+          4: false,
+          3: false,
+          2: false,
+          1: false
+        }
+      }
+    }
   };
-  handleClick = op => {
-    this.setState({
-      show: !this.state.show,
-      operator: op
-    });
+
+  opClick = op => {
+    this.setState(prevState => ({
+      show: !prevState.show,
+      selectedOp: op
+    }));
   };
+
   render() {
     return (
       <div className="App">
         <Header />
         <main className="wrapper">
 
+          { // types of filters
+            Object.entries(this.state.filterTags).map((filters) => {
+              // console.log(filters[1]);
+              return (
+                <ul className="filter-list" key={filters[0]}>
+                  { // individual filters
+                    Object.entries(filters[1]).map(([key, value]) => {
+                      // console.log(key, value);
+                      return (
+                        <li className="filter-element" key={key} data-selected={value}>{key + (filters[0] === 'rarity' ? "\u2606" : "")}<div className="overlay"></div></li>
+                      );
+                    })}
+                </ul>
+              );
+            })}
+
           <ul className="operator-list">
 
             {operators.map((operator) => {
 
               return (
-                <li className="list-element" key={operator.name} onClick={() => { this.handleClick(operator); }}>
+                <li className="op-list-element" key={operator.name} onClick={() => { this.opClick(operator); }}>
                   <img src={baseUrl + operator.icon} alt={operator.name} className={`icon rarity-${operator.stats.rarity}`} />
                   <div className="overlay"></div>
                 </li>
@@ -37,9 +78,9 @@ class App extends React.Component {
           </ul>
 
           <Modal
-            onClose={this.handleClick}
+            onClose={this.opClick}
             show={this.state.show}
-            op={this.state.operator} />
+            op={this.state.selectedOp} />
         </main>
       </div>
     );
